442b8308d2f6ecbe4378b12d9f58d47f7c49daed
tienvm1 <tienvm1kaopiz.com>
2021-12-15 17:45:36 +0700

    Chapter 4: Split layout

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
-// const http = require('http')
-// const server = http.createServer((req, res) => {
-//   if (req.url === '/about')
-//     res.end('The about page')
-//   else if (req.url === '/contact')
-//     res.end('The contact page')
-//   else if (req.url === '/')
-//     res.end('The home page')
-//   else {
-//     res.writeHead(404)
-//     res.end('page not found')
-//   }
-// })
-// server.listen(3333);
-
-// const express = require('express')
-// const path = require('path')
-// const app = express()
-
 const express = require('express');
 const path = require('path');
 const app = express()
 
-// const sum = require('./Addition.js');
+const ejs = require('ejs')
+app.set('view engine','ejs')
 
 app.use(express.static('public'));
 
@@ -30,20 +12,23 @@ app.listen(3333, (request, response) => {
 });
 
 app.get("/", (req, res) => {
-  // console.log(sum());
-  res.sendFile(path.resolve(__dirname,'pages/index.html'))
+  // res.sendFile(path.resolve(__dirname,'pages/index.html'))
+  res.render('index');
 })
 
 app.get('/about',(req,res)=>{
-  res.sendFile(path.resolve(__dirname,'pages/about.html'))
+  // res.sendFile(path.resolve(__dirname,'pages/about.html'))
+  res.render('about');
 })
 
 app.get('/contact',(req,res)=>{
-  res.sendFile(path.resolve(__dirname,'pages/contact.html'))
+  // res.sendFile(path.resolve(__dirname,'pages/contact.html'))
+  res.render('contact');
 })
 
 app.get('/post',(req,res)=>{
-  res.sendFile(path.resolve(__dirname,'pages/post.html'))
+  // res.sendFile(path.resolve(__dirname,'pages/post.html'))
+  res.render('post');
 })
 
 app.get('*', function (req, res) {
