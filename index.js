c0b6984e38ae72170cde59ea0b3662da4edcbe06
tienvm1 <tienvm1kaopiz.com>
2022-07-04 17:46:42 +0700

    Chap 5, 6: Mongo Db - APPLYING MONGODB TO OUR PROJECT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 const BlogPost = require('./models/BlogPost')
 
@@ -10,6 +11,8 @@ const ejs = require('ejs')
 app.set('view engine','ejs')
 
 app.use(express.static('public'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
 
 mongoose.connect('mongodb://localhost:27017', {useNewUrlParser: true});
@@ -18,9 +21,15 @@ app.listen(3333, (request, response) => {
   console.log("App listening on port 3333")
 });
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   // res.sendFile(path.resolve(__dirname,'pages/index.html'))
-  res.render('index');
+
+  // res.render('index');
+
+  const blogposts = await BlogPost.find({})
+  res.render('index',{
+    blogposts: blogposts
+  });
 })
 
 app.get('/about',(req,res)=>{
@@ -33,18 +42,72 @@ app.get('/contact',(req,res)=>{
   res.render('contact');
 })
 
-app.get('/post',(req,res)=>{
+app.get('/post/:id', async (req,res)=>{
   // res.sendFile(path.resolve(__dirname,'pages/post.html'))
-  res.render('post');
+  // res.render('post')
+
+  const blogpost = await BlogPost.findById(req.params.id)
+  res.render('post',{
+    blogpost
+  })
 })
 
-app.get('/create',(req,res)=>{
-  BlogPost.create({
-    title: 'this is title',
-    body: `If you`,
-}, (error, blogpost) =>{
-    console.log(error,blogpost)
-  })
+app.get('/posts/new',(req,res)=>{
+  // res.sendFile(path.resolve(__dirname,'pages/post.html'))
+  res.render('create');
+})
+
+app.post('/posts/store',async(req,res)=>{
+  console.log(req.body);
+  // BlogPost.create(req.body,(error,blogpost) =>{
+  //   res.redirect('/')
+  // })
+
+  await BlogPost.create(req.body)
+  res.redirect('/')
+})
+
+app.get('/test-mongodb',(req,res)=>{
+//   BlogPost.create({
+//     title: 'this is title',
+//     body: `If you`,
+// }, (error, blogpost) =>{
+//     console.log(error,blogpost)
+//   })
+
+  // BlogPost.find({}, (error, blogspot) =>{
+  //   console.log(error,blogspot)
+  // })
+
+  // BlogPost.find({
+  //   title:'this is title'
+  // }, (error, blogspot) =>{
+  //   console.log(error,blogspot)
+  // })
+  //
+  // BlogPost.find({
+  //   title: /this is/
+  // }, (error, blogspot) =>{
+  //   console.log(error,blogspot)
+  // })
+
+  // const id = "61baf1a96d6fd46add8b6c8f";
+  // BlogPost.findById(id, (error, blogspot) =>{
+  //   console.log(error,blogspot)
+  // })
+
+  // const id = "62bece81d1108fd764a81d02";
+  // BlogPost.findByIdAndUpdate(id,{
+  //   title:'this is title - Updated title'
+  // }, (error, blogspot) =>{
+  //   console.log(error,blogspot)
+  // })
+
+  // var id = "5cb436980b33147489eadfbb";
+  // BlogPost.findByIdAndDelete(id, (error, blogspot) =>{
+  //   console.log(error,blogspot)
+  // })
+
   res.render('post');
 })
 
--- models/BlogPost.js
+++ models/BlogPost.js
@@ -4,7 +4,12 @@ const Schema = mongoose.Schema;
 
 const BlogPostSchema = new Schema({
   title: String,
-  body: String
+  body: String,
+  username: String,
+  datePosted:{ /* can declare property type with an object like this because we need 'default' */
+    type: Date,
+    default: new Date()
+  }
 });
 
 const BlogPost = mongoose.model('BlogPost',BlogPostSchema);
