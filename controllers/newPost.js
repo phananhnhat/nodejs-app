3e11c6a0c00b3a99a4822d47feadc1c66d8cb3a4
tienvm1 <tienvm1kaopiz.com>
2022-07-08 15:07:21 +0700

    CHAPTER 14: ADDING A WYSIWYG EDITOR

--- controllers/newPost.js
+++ controllers/newPost.js
@@ -1,7 +1,9 @@
 module.exports = (req, res) => {
     // res.sendFile(path.resolve(__dirname,'pages/post.html')) // => Khi di chuyển từ index.js vào thư mục controller thì path sẽ ko chính xác nữa
    if(req.session.userId){
-      return res.render("create");
+      return res.render("create", {
+         createPost: true,
+      });
    }
    res.redirect('/auth/login')
 }
